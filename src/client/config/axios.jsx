31ee76d4a6e46469f4d39c8b4c6240416a5518e3
btee9910 <btee9910@gmail.com>
2023-09-07 15:46:36 +1000

    deploy v1

--- src/client/config/axios.jsx
+++ src/client/config/axios.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-
-
 const axiosConfig = axios.create({
   // .. configure axios baseURL
-  baseURL: "http://localhost:3000/"
+  // baseURL: "http://localhost:3000/"
+  baseURL: "/"
+
 });
 
 
