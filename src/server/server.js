60762f275a134073831247e6bfca486447d8a0a0
Brandon Tee <128451789+btee9910@users.noreply.github.com>
2025-07-31 16:49:39 +1000

    update node

--- src/server/server.js
+++ src/server/server.js
@@ -1,55 +1,50 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import routes from './routes/index.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import routes from "./routes/index.js";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT;
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => {
-        console.log("MongoDB connected")
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    }).catch((error) => {
-        console.log(error)
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // express to support json
 
-routes(app); // attach our routes to the servers 
-
-// a 404 "page not found" 
-app.use((req, res) => {  // default function if not function work
-    res.status(404).send({ url: `${req.originalUrl} not found` });
-    console.log(`Server running on http://localhost:${PORT}`);
-
-});
-
-// avoid access to any other route
-app.use("*", (req, res) => {
-    res.status(404).json({
-        success: "false",
-        message: "Page not found",
-        error: {
-            statusCode: 404,
-            message: "You reached a route that is not defined on this server",
-        },
-    });
-});
+routes(app); // attach our routes to the servers
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// a 404 "page not found"
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
 });
 
 export default app;
