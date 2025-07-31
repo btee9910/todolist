cbcbf1c33536dc78f411bc59063246772f7101f5
Brandon Tee <128451789+btee9910@users.noreply.github.com>
2025-07-31 18:24:12 +1000

    fix server routes

--- src/server/server.js
+++ src/server/server.js
@@ -31,11 +31,14 @@ app.use(express.json()); // express to support json
 
 routes(app); // attach our routes to the servers
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+// a 404 "page not found"
+app.use((req, res) => {
+  // default function if not function work
+  res.status(404).send({ url: `${req.originalUrl} not found` });
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
-// a 404 "page not found"
+// avoid access to any other route
 app.use("*", (req, res) => {
   res.status(404).json({
     success: "false",
@@ -45,6 +48,10 @@ app.use("*", (req, res) => {
       message: "You reached a route that is not defined on this server",
     },
   });
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 export default app;
